ce2de621f1bf4278ffe32ed86300b5f2cd274cca
Waleed A <121586985+waleedA13@users.noreply.github.com>
2026-06-03 20:45:45 -0700

    fix(lint): add braces to if statement in api-events-filters.spec.js

--- tests/e2e/api-events-filters.spec.js
+++ tests/e2e/api-events-filters.spec.js
@@ -63,7 +63,7 @@ test.describe("Prototype 3 event APIs", () => {
   });
 
   test.afterAll(async () => {
-    if (api) await api.dispose();
+    if (api) { await api.dispose(); }
   });
 
   test("GET /api/events response is shaped { events: [...] } and is JSON", async () => {
